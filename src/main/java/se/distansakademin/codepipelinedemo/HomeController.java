090c8258188840b7ea7575880a822d7746744c95
JentrixAhl <jentyatrix@gmail.com>
2023-09-20 13:13:38 +0200

    added changes

---
+++ src/main/java/se/distansakademin/codepipelinedemo/HomeController.java
@@ -0,0 +1,12 @@
+package se.distansakademin.codepipelinedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getHome(){
+        return "home";
+    }
+}
